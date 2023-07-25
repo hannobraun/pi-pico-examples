9ba157a0380f3e933bea6ee845d9793282424e37
Hanno Braun <hanno@braun-odw.eu>
2023-07-25 10:22:41 +0200

    Remove redundant `build.rs`

--- getting-started/build.rs
+++
@@ -1,8 +0,0 @@
-use std::{env, fs, path::PathBuf};
-
-fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let target = PathBuf::from(out_dir).join("memory.x");
-
-    fs::copy("memory.x", target).unwrap();
-}
