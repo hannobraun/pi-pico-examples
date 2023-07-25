9684833aa91338080f156d97bffb8ffdb47bceb5
Hanno Braun <hanno@braun-odw.eu>
2023-07-25 11:08:02 +0200

    Blink LED

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -5,6 +5,7 @@
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{Level, Output};
+use embassy_time::{Duration, Timer};
 use panic_probe as _;
 
 #[embassy_executor::main]
@@ -12,9 +13,15 @@ async fn main(_spawner: Spawner) {
     defmt::info!("Initializing...");
 
     let peripherals = embassy_rp::init(Default::default());
-    let _led = Output::new(peripherals.PIN_25, Level::High);
+    let mut led = Output::new(peripherals.PIN_25, Level::Low);
 
     defmt::info!("Initialized.");
 
-    loop {}
+    loop {
+        led.set_high();
+        Timer::after(Duration::from_millis(50)).await;
+
+        led.set_low();
+        Timer::after(Duration::from_millis(950)).await;
+    }
 }
