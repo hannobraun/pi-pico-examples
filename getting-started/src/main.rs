df637a4896179b11e516426af30eb3378065c6d8
Hanno Braun <hanno@braun-odw.eu>
2023-07-25 10:46:41 +0200

    Add log messages

--- getting-started/src/main.rs
+++ getting-started/src/main.rs
@@ -9,8 +9,12 @@ use panic_halt as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    defmt::info!("Initializing...");
+
     let peripherals = embassy_rp::init(Default::default());
     let _led = Output::new(peripherals.PIN_25, Level::High);
+
+    defmt::info!("Initialized.");
 
     loop {}
 }
