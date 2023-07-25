678ede5fe0a11807a317901613627180fd9fe8ee
Hanno Braun <hanno@braun-odw.eu>
2023-07-25 10:22:22 +0200

    Pass linker args via `.cargo/config.toml`

--- getting-started/build.rs
+++ getting-started/build.rs
@@ -5,7 +5,4 @@ fn main() {
     let target = PathBuf::from(out_dir).join("memory.x");
 
     fs::copy("memory.x", target).unwrap();
-
-    println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
 }
